cf4d45d7f82a02725288a3a2a7b0d0cd9461103e
drice89 <drice89@gmail.com>
2020-04-27 17:58:49 -0700

    Added route utils

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -7,7 +7,7 @@ const sessionReducer = (state = _nullSession, action) => {
 
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return { id: action.user.id }
+      return { id: action.user.id };
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default: 
