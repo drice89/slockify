c714994b9d9009ce68f99a69d1538985cd0d7c88
drice89 <drice89@gmail.com>
2020-04-29 00:35:55 -0700

    Added more styling to splash page

--- README.md
+++ README.md
@@ -23,3 +23,9 @@ Things you may want to cover:
 
 * ...
 # slockify
+
+Public Domain images used
+https://www.pexels.com/photo/man-in-black-crew-neck-shirt-wearing-black-framed-eyeglasses-4212883/
+https://www.pexels.com/photo/shallow-focus-photo-of-woman-wearing-black-beanie-3804725/
+https://www.pexels.com/photo/woman-in-black-fur-coat-wearing-white-headphones-3786670/
+https://www.pexels.com/photo/man-wearing-denim-jacket-singing-on-stage-894156/
--- frontend/components/splash/splash_body/description_component.jsx
+++ frontend/components/splash/splash_body/description_component.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 
+
 const DescriptionComponent = () => (
-  <section>
-    <div className="container-med">
-      <div>COLLABORATE ON PLAYLISTS</div>
-      <div>Slockify brings the music lovers together, wherever they are</div>
-      <div>Whether you are a musician or a music lover, collaborative playlist channels and live chat allow you to share your music.</div>
-      <div><button>Learn More</button><button>Contact Us</button></div>
-    </div>
+  <section className="description-container">
+    <div>
       <div className="container-med">
-       Image
+        <div className="small-caps">LISTEN, TOGETHER</div>
+        <div><strong><h1>Slockify brings the music lovers together through live community chat and collaborative playlists</h1></strong></div>
+        <div>Whether you are a musician or a music lover, collaborative playlist channels and live chat allow you to share your music.</div>
+        <div>
+          <a href="https://github.com/drice89"><button className="cta-button">LEARN MORE</button></a>
+          <a href="mailto:dillon.m.rice@gmail.com">< button className="cta-button cta-inverse">CONTACT US</button></a>
+        </div>
       </div>
+        <div className="container-med">
+        <img className="hero-image" src={window.imageHero} />
+        </div>
+    </div>
     <div className="container-large">
-      <div>Break out of Music Forums</div>
-      <div>Collaborating and communicating in channels allows for real time distribution of new music</div>
-      <div>Stock video placeholder</div>
+      <div><h1>Break out of Music Forums</h1></div>
+      <div>Collaborating and communicating in channels allows for real time distribution and discussion of new music</div>
+      <div><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Hw30__NzVTc?controls=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>
     </div>
   </section>
 )
