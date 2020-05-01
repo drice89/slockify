38f45f70952c0bdf8a0c034788fa00beefb43e21
drice89 <drice89@gmail.com>
2020-05-01 11:38:02 -0700

    Fixed controller nesting - commit is fall back before I destroy the old controllers- updated

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,44 @@
 class Api::MessagesController < ApplicationController
+  def index
+    @messages = Messages.find_by(recipient_id: params[:recipient_id])
+    render :show
+  end
+
+  def show
+    @message = Message.find_by(id: params[:id])
+    if @message
+      render :show
+    else
+      render json: ["Message not found"], status: 404
+    end
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @message = Message.find_by(id: params[:id])
+    if @message.update(message_params)
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @message = Message.find_by(id: params[:id])
+    @message.destroy
+    render :show
+  end
+
+  private 
+  def message_params
+    params.requires(:message).permit(:body, :author_id, :recipient_id))
+  end
 end
