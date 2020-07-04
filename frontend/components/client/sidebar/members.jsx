09cf47801c54c33930300a95cafc93ba8d668c82
Dillon Rice <drice89@gmail.com>
2020-07-03 23:34:36 -0700

    Change styling for buttons and members tab

--- frontend/components/client/sidebar/members.jsx
+++ frontend/components/client/sidebar/members.jsx
@@ -8,9 +8,12 @@ const mapDispatchToProps = (dispatch) => ({
 
 const Members = ({users, activeTab, openModal, conversation}) => {
   return (
-    <div className={activeTab === "members" || activeTab === "group" ? "" : "hidden"}>
+    <div className={activeTab === "members" ? "members-section" : "hidden"}>
       { conversation.memberIds.map(id => (
-        <div key={users[id].fullName}>{users[id].displayName || users[id].fullName}</div>
+        <div key={users[id].fullName}>
+          <span><img src={users[id].avatarUrl || window.defaultAvatarUrl}></img></span>
+          <span>{users[id].displayName || users[id].fullName}</span>
+        </div>
       ))}
       <div><button onClick={() => openModal("members")}>Add Member</button></div>
     </div>
